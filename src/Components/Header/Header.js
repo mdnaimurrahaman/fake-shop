b18ea9295ef846ec7fc20175be112949b1d77416
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-07-17 16:33:54 +0600

    Projects Initialisation

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {AiOutlineAppstoreAdd} from 'react-icons/ai'
+import {AiFillShop} from 'react-icons/ai'
+import {HiShoppingCart} from 'react-icons/hi'
+import {RiUser3Fill} from 'react-icons/ri'
+import './Header.css'
+
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+        
+      </Navbar>
+    );
+};
+
+export default Header;
