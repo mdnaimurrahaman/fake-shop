3999a7de9b397633a172f49d1c68dc05c9808ccb
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-07-17 16:35:22 +0600

    Navber added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -12,7 +12,18 @@ import './Header.css'
 const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
-        
+        <Container>
+          <Navbar.Brand className='brand__logo' href="#home"><AiFillShop/> FakeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="#link"><RiUser3Fill/></Nav.Link>
+              <Nav.Link href="#link"><HiShoppingCart/></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     );
 };
